f8901cd7299db89ae010b4ac33df524a83465e8d
Jake <jakespeitel19@gmail.com>
2022-05-26 15:17:02 -0400

    commit tree 716a99cd9b5c7b5cc01d271d733323219b2f98a2

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+function countDown(posInt){
+    if(posInt > 0){
+       
+        let newInt = posInt;
+
+        while(newInt > -1){
+            
+            console.log(newInt);
+            
+            newInt = newInt - 1
+        }
+    }
+}
+
+
+function writeCards(person, event){
+    
+    let newArray = [];
+    
+    for(let numberOfPeople = 0; numberOfPeople < person.length; numberOfPeople++){
+       
+        newArray[numberOfPeople] = "Thank you, " + person[numberOfPeople] + ", for the wonderful " + event + " gift!";
+    }
+    return newArray;
+}    
